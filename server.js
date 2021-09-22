e5fb163a890a3d342a6bda35817a6096caa3782e
Srinath <srinath.subramanian1@gmail.com>
2021-09-22 15:56:38 -0500

    updating database connection

--- server.js
+++ server.js
@@ -11,11 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   	client: 'pg',
   	connection: {
-    host : 'postgresql-rugged-50427',
-    port : 5432,
-    user : 'postgres',
-    password : '',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl:true
   	}
 });
 
